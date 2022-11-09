d8900579401c3a6a4a202b3fdb92a2ee6ee93e2d
Turan98 <48137040+Turan98@users.noreply.github.com>
2022-11-09 13:43:58 +0400

    test

---
+++ triangle/Program14.cs
@@ -0,0 +1,52 @@
+﻿int a;
+Console.Write("Enter height of triangle: ");
+a = Convert.ToInt32(Console.ReadLine());
+for (int i = 0; i < a; i++)
+{
+    for (int k = 0; k <= i; k++)
+    {
+        Console.Write(" ");
+    }
+    for (int j = a; j > i; j--)
+    {
+        Console.Write("#");
+    }
+    Console.WriteLine();
+}
+
+for (int i = 0; i < a; i++)
+{
+    for (int k = a; k>i; k--)
+    {
+        Console.Write(" ");
+    }
+    for (int j = 0; j <= i; j++)
+    {
+        Console.Write("#");
+    }
+    Console.WriteLine();
+}
+Console.WriteLine();
+for (int i = 0; i < a; i++)
+{
+    
+    for (int j = 0; j <=i; j++)
+    {
+        Console.Write("#");
+    }
+    Console.WriteLine();
+}
+for (int i = 0; i < a; i++)
+{
+    for (int k = 0; k < i; k++)
+    {
+        
+    }
+    for (int j = a; j >i; j--)
+    {
+        Console.Write("#");
+    }
+    Console.WriteLine();
+}
+
+
